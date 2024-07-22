20edf7af1d5842da675c6fad302e753e7460c9b7
hattmt <56161715+hattmt@users.noreply.github.com>
2024-07-22 23:35:31 +0200

    Update STM32G0B1_ADC.cs

--- stm32g0b1/STM32G0B1_ADC.cs
+++ stm32g0b1/STM32G0B1_ADC.cs
@@ -1,8 +1,6 @@
 //
-// Copyright (c) 2010-2024 Antmicro
-//
-// This file is licensed under the MIT License.
-// Full license text is available in 'licenses/MIT.txt'.
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
 //
 
 using System;
